d3f0cc392ebe82de1b32f5bb6a7ee7728dd82930
Aniket <aniketkule231997@gmail.com>
2023-12-10 17:11:45 +0530

    Initial commit

---
+++ src/Components/Common/Footer/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function index() {
+  return (
+    <div className="footer">
+        <p>footer</p>
+    </div>
+  )
+}
+
+export default index
---
+++ src/Components/Common/Header/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function index() {
+  return (
+    <div className='header'>
+        <p>header</p>
+    </div>
+  )
+}
+
+export default index
