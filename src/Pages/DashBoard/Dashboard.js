bb1b042215969211bdb494c1a5ddf00e665bb615
Aniket Kule <aniketkule231997@gmail.com>
2023-12-17 12:03:46 +0530

    newchangesmac

--- src/Pages/DashBoard/Dashboard.js
+++ src/Pages/DashBoard/Dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Header from '../../Components/Common/Header/index'
+import DashPage from '../DashBoard/DashPage.js'
+import axios from "axios";
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+
+const DashBoard = ()=>{
+    const [coins, setCoins]=useState([]);
+
+    useEffect(()=>{
+      axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
+      .then((res)=>setCoins(res.data))
+      .catch((err)=>console.log(err))
+    },[])
+
+      return(
+        <div>
+          <Header />
+          <DashPage coins={coins}/>
+
+        </div>
+
+      )
 }
 
-export default Dashboard
+export default DashBoard;
