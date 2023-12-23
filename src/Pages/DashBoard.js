99243f5de3512435c7aa305c29a97dafdab1165e
Aniket <aniketkule231997@gmail.com>
2023-12-23 18:03:07 +0530

    u

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -7,11 +7,13 @@ import Search from './DashBoard/Tab/Search/index.js';
 
 const DashBoard = ()=>{
     const [coins, setCoins]=useState([]);
-    const [search, setSearch]=useState(null);
+    const [search, setSearch]=useState('');
 
     const onChangeSearch=(value)=>{
       setSearch(value);
     }
+
+    const filteredval= coins.filter((item)=>item.name.toLowerCase().includes(search.toLowerCase()));
 
     useEffect(()=>{
       axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
@@ -23,7 +25,7 @@ const DashBoard = ()=>{
         <div>
           <Header />
           <Search search={search} onChangeSearch={onChangeSearch} />
-          <DashPage coins={coins}/>
+          <DashPage coins={filteredval}/>
 
         </div>
 
