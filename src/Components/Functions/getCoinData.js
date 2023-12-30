d1ed30f79b2f2f04dceee982aa84ee6fa16cfbf6
Aniket <aniketkule231997@gmail.com>
2023-12-30 19:27:06 +0530

    uu

--- src/Components/Functions/getCoinData.js
+++ src/Components/Functions/getCoinData.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export const getCoinData= (id)=>{
-    const myData= axios.get(`https://api.coingecko.com/api/v3/coins/${id}`)
-    .then((response)=>{console.log(response.data);
-       return response.data
-})
-    .catch((err)=>{console.log(err.response.data);
-     return  myData;
-})
+ const myData=  axios.get(`https://api.coingecko.com/api/v3/coins/${id}`)
+            .then((response)=>{
+                 return response.data;
+        })
+            .catch((err)=>{
+                console.log(err.response.data); 
+        })
+
+        return myData;
 }
--- src/Components/Functions/getCoinPrices.js
+++ src/Components/Functions/getCoinPrices.js
@@ -5,8 +5,8 @@ export const getCoinPrices=(id, day)=>{
     (`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${day}&interval=daily`)
     .then((response)=>{
     console.log("response>>>", response.data.prices)
-     return response.data;}
+     return response.data.prices;}
     )
     .catch((err)=>console.log(err));
      return prices;
-}
+}  
