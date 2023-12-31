a9b3b1408ab3be746eec48f0d8afb72ee141a0fc
Aniket <aniketkule231997@gmail.com>
2023-12-31 13:43:16 +0530

    latest

--- src/Pages/Coin/SelectDays/index.js
+++ src/Pages/Coin/SelectDays/index.js
@@ -7,16 +7,15 @@ import Select from '@mui/material/Select';
 import './style.css';
 
 export default function SelectDays() {
-  const [day, setDay] = React.useState('');
+  const [day, setDay] = React.useState(7);
 
   const handleChange = (event) => {
     setDay(event.target.value);
+    console.log(event.target.value)
   };
 
   let style={
     color:"var(--black)",
-
-
   }
 
   return (
